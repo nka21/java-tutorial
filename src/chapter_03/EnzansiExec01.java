d246f85ac57c863e2973e1b1129ab5221facff02
nka21 <naoki.nico@gmail.com>
2024-07-20 17:52:10 +0900

    input: インクリメントとデクリメント

---
+++ src/chapter_03/EnzansiExec01.java
@@ -0,0 +1,14 @@
+package chapter_03;
+
+public class EnzansiExec01 {
+    /**
+     * mainメソッド
+     */
+    public static void main(String[] args) {
+        int n = 0, m = 0; // n,mには0を代入しておく。
+        n++; // インクリメント
+        m--; // デクリメント
+        System.out.println(n);
+        System.out.println(m);
+    }
+}
