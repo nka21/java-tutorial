70d6aca047ab2e8e6c7938580a72317dbbedc817
nka21 <naoki.nico@gmail.com>
2024-07-20 18:08:21 +0900

    input: 文字列の結合

---
+++ src/chapter_03/EnzansiExec04.java
@@ -0,0 +1,11 @@
+package chapter_03;
+
+public class EnzansiExec04 {
+    /**
+     * 文字列の連結
+     */
+    public static void main(String[] args) {
+        String str = "おはよう" + "佐藤" + "さん";
+        System.out.println(str);
+    }
+}
