e2b8c1a68b4d3b04b7c18d4f435bfb4596021131
nka21 <naoki.nico@gmail.com>
2024-07-20 10:12:01 +0900

    chore: Update self-introduction in naoki.java

---
+++ src/chapter_01/task/naoki.java
@@ -0,0 +1,29 @@
+package chapter_01.task;
+
+/**
+ * 自己紹介クラス
+ * @version 1.0
+ * @author naoki
+ */
+public class naoki {
+  /**
+  * mainメソッド
+  * @param args 無し
+  */
+    public static void main(String[] args) {
+        System.out.println("自己紹介をおこないます");
+        System.out.println("=========================");
+        System.out.println("名前：naoki");
+        System.out.println("出身：Tokushima");
+        System.out.println("趣味：ご飯食べること");
+        System.out.println("特技：ゲーム");
+        System.out.println("ニックネーム：naoki");
+        System.out.println("いま欲しい物：オフィスチェア");
+        System.out.println("好きな食べ物：肉");
+        System.out.println("嫌いな食べ物：ない");
+        System.out.println("行ってみたい場所：北海道");
+        System.out.println("今年の目標：勉強頑張る");
+        System.out.println("今後の抱負：健康に生きる");
+        System.out.println("=========================");
+    }
+}
