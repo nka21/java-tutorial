a283faebce0dcfb0c823cbb26188f94afb3062ec
nka21 <naoki.nico@gmail.com>
2024-07-20 18:38:38 +0900

    input: 複合代入演算子(キャスト不要)

---
+++ src/chapter_03/EnzansiExec09.java
@@ -0,0 +1,13 @@
+package chapter_03;
+
+public class EnzansiExec09 {
+    public static void main(String[] args) {
+
+        int n = 0;
+
+        // double型はint型へ代入するがエラーにならない
+        n += 15.8;
+        System.out.println(n); // 15
+
+    }
+}
