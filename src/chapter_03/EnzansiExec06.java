f917b59c8daeb7843340413755d376e4ac9a1afa
nka21 <naoki.nico@gmail.com>
2024-07-20 18:11:02 +0900

    input: 算術演算子

---
+++ src/chapter_03/EnzansiExec06.java
@@ -0,0 +1,22 @@
+package chapter_03;
+
+public class EnzansiExec06 {
+    public static void main(String[] args) {
+        // 加減乗算
+        int a = 20;
+        int b = 30;
+        int a1, a2, a3, a4, a5;
+
+        a1 = a + b;
+        a2 = a - b;
+        a3 = a * b;
+        a4 = a / b;
+        a5 = a % b;
+
+        System.out.println("a1 = " + a1);
+        System.out.println("a1 = " + a2);
+        System.out.println("a1 = " + a3);
+        System.out.println("a1 = " + a4);
+        System.out.println("a1 = " + a5);
+    }
+}
