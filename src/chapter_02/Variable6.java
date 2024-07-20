17c9592a61bdf7d2082bf75c6a1e8100dca2c673
nka21 <naoki.nico@gmail.com>
2024-07-20 10:48:15 +0900

    chore: 文字列の学習

---
+++ src/chapter_02/Variable6.java
@@ -0,0 +1,9 @@
+package chapter_02;
+
+public class Variable6 {
+  public static void main(String[] args) {
+    System.out.println("ああああ"); //文字列
+    System.out.println("AAAAAA");   //文字列
+    System.out.println("");         //空の文字列
+}
+}
