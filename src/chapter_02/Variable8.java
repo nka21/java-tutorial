709933e2691e0d44d346a7329c5142f7fb460b89
nka21 <naoki.nico@gmail.com>
2024-07-20 10:53:36 +0900

    chore: double型を用いた、計算

---
+++ src/chapter_02/Variable8.java
@@ -0,0 +1,16 @@
+package chapter_02;
+
+public class Variable8 {
+  public static void main(String[] args) {
+
+    double number = 10; // 10.0へ暗黙の型変換されて、numberへ代入
+
+    double x = 3.5;
+    double y = x + 2; // 2は2.0へ暗黙の型変換されて計算に使われる
+
+    // int z = 12.345; // コンパイルエラーになる
+
+    System.out.println(number);
+    System.out.println("y:" + y);
+  }
+}
