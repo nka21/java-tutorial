624f103e24fbb8cc5ccf5daf1dde825957bdb73a
nka21 <naoki.nico@gmail.com>
2024-07-20 10:43:51 +0900

    chore: double型とfloat型について学習

---
+++ src/chapter_02/Variable4.java
@@ -0,0 +1,25 @@
+package chapter_02;
+
+public class Variable4 {
+  public static void main(String[] args) {
+    // 浮動小数点型
+    // 扱える浮動小数点型の大きさによって、double型とfloat型に分けられる
+
+    /*
+     * double型とは
+     * 64bitで表現
+    */
+
+    /*
+      * float型とは
+      * 32bitで表現
+    */
+    System.out.println(12.34); // 10進数表記
+    System.out.println(12.34f); // 10進数表記(float型)
+    System.out.println(12.34f); // 10進数表記(float型)
+    System.out.println(12.34d); // 10進数表記(double型)
+    System.out.println(12.34D); // 10進数表記(double型)
+    System.out.println(1234e-2); // 指数表記
+
+  }
+}
