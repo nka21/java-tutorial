6b08d75dd91814e0cc9c58749e8ea3be8da3e044
nka21 <naoki.nico@gmail.com>
2024-07-20 10:07:53 +0900

    chore: コメントの練習

---
+++ src/chapter_01/HelloJon.java
@@ -0,0 +1,21 @@
+package chapter_01;
+
+/**
+ * ドキュメンテーションコメントと呼ばれるもの
+ * APIの仕様書
+ *
+ * HelloJonと3回実行するメソッド
+ * @version 1.0
+ * @author naoki
+ */
+public class HelloJon {
+    /*
+     * 複数行コメント
+     * わー
+     */
+    public static void main(String[] args) {
+        System.out.print("HelloJon");
+        System.out.print("HelloJon");
+        System.out.print("HelloJon");
+  }
+}
