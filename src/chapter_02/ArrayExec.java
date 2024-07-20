04560cd3b02ce45732bc2362c98e7fd9b67aff38
nka21 <naoki.nico@gmail.com>
2024-07-20 11:07:32 +0900

    input: 多次元配列

--- src/chapter_02/ArrayExec.java
+++ src/chapter_02/ArrayExec.java
@@ -18,5 +18,20 @@ public class ArrayExec {
     System.out.println(c[0]); // お
     System.out.println(d[0]); // おはよう
     System.out.println(e[0]); // true
+
+    // 多次元配列の利用
+    int[][] x = { { 10, 2, 4, 9, 8, 7, 5 }, { 7, 2, 5, 4, 3 } };
+    System.out.println(x[0][0]); // 10
+    System.out.println(x[0][1]); // 2
+    System.out.println(x[0][2]); // 4
+    System.out.println(x[0][3]); // 9
+    System.out.println(x[0][4]); // 8
+    System.out.println(x[0][5]); // 7
+    System.out.println(x[0][6]); // 5
+    System.out.println(x[1][0]); // 7
+    System.out.println(x[1][1]); // 2
+    System.out.println(x[1][2]); // 5
+    System.out.println(x[1][3]); // 4
+    System.out.println(x[1][4]); // 3
   }
 }
