f0d4f06020ef1696b61cbf9cef12950823aaa9ba
nka21 <naoki.nico@gmail.com>
2024-07-20 18:14:32 +0900

    input: 代入演算子

---
+++ src/chapter_03/EnzansiExec07.java
@@ -0,0 +1,15 @@
+package chapter_03;
+
+public class EnzansiExec07 {
+    public static void main(String[] args) {
+        /*
+         * 変数 = 式;
+         */
+        int a, b, c;
+        a = b = c = 5; // a,b,cに5を代入する
+
+        System.out.println("a= " + a);
+        System.out.println("a= " + b);
+        System.out.println("a= " + c);
+    }
+}
