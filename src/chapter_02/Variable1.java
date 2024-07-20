11a6243c2de3fd6931aeb3f2631c1e24c840cfb1
nka21 <naoki.nico@gmail.com>
2024-07-20 10:37:20 +0900

    chore: 型や変数の学習

---
+++ src/chapter_02/Variable1.java
@@ -0,0 +1,19 @@
+package chapter_02;
+
+/**
+ * 実行するためのクラス
+ * @version 1.0
+ * @author naoki
+ */
+public class Variable1 {
+    /**
+    * mainメソッド
+    */
+    public static void main(String[] args) {
+        int number; // int型の変数numberを宣言
+        number = 10; // numberに10を代入
+        number = 20; // numberに20を再代入
+        number = number + 1; // numberに1を足す
+        System.out.println(number); // numberを表示する
+    }
+}
