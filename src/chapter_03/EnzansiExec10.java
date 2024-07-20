928e6e833f37b4e3685f579fb40664a1280a13cd
nka21 <naoki.nico@gmail.com>
2024-07-20 18:40:57 +0900

    input: 関係演算子

---
+++ src/chapter_03/EnzansiExec10.java
@@ -0,0 +1,24 @@
+package chapter_03;
+
+public class EnzansiExec10 {
+    public static void main(String[] args) {
+
+        int num0 = Integer.parseInt(args[0]);
+        System.out.println(num0 > 50);
+
+        // a > 0 //aは正の数
+        // a >= 10 //aは10以上
+        // a == 20 //aは20と等しい
+        // a != 0 //aは0ではない
+
+        // a > b+5 //aはb+5よりも大きい
+        // a > b //aはbよりも大きい
+        // a+b > 5 //a+bは5以上
+        // a+b > c //a+bはc以上
+        // a == b*2 //aはbの2倍と等しい
+        // a%3 == 0 //aはbの3倍である
+        // a+1 != 2 //a+1は2ではない
+        // a+1 != b-2 //a+1はb-2ではない
+
+    }
+}
