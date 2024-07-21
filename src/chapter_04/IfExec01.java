71a55ca1fbd7beae79436f24336a51954438121d
nka21 <naoki.nico@gmail.com>
2024-07-21 17:44:26 +0900

    input: if文

---
+++ src/chapter_04/IfExec01.java
@@ -0,0 +1,12 @@
+package chapter_04;
+
+public class IfExec01 {
+    public static void main(String[] args) {
+
+        int i = Integer.parseInt(args[0]);
+
+        if (i < 5) {
+            System.out.println(i + "は5未満です。");
+        }
+    }
+}
