706d43858ceb17345d3b786f02bf9e1eef05281f
nka21 <naoki.nico@gmail.com>
2024-07-21 18:03:57 +0900

    input: for文

---
+++ src/chapter_04/ForExec.java
@@ -0,0 +1,10 @@
+package chapter_04;
+
+public class ForExec {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
