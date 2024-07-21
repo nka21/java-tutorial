267524350ae16b3e92c6e21195658764c4fb099b
nka21 <naoki.nico@gmail.com>
2024-07-21 17:20:21 +0900

    output: OperatorExec

---
+++ src/chapter_03/task/OperatorExec.java
@@ -0,0 +1,14 @@
+package chapter_03.task;
+
+public class OperatorExec {
+    public static void main(String[] args) {
+
+        int n = 10;
+        double x = 2.5;
+
+        System.out.println(n + "/" + 3);
+        System.out.println(n + 1 + "/" + 3);
+        System.out.println("number is " + (x + n));
+
+    }
+}
