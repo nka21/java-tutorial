eb2552ba5e5d76609ed1d536efc6e171317ee51a
nka21 <naoki.nico@gmail.com>
2024-10-16 16:54:39 +0900

    異なったパッケージによるpublic修飾子の有無

---
+++ src/chapter_07/ch7A/Employee.java
@@ -0,0 +1,12 @@
+package chapter_07.ch7A;
+
+public class Employee {
+    public String empName;
+
+    public int empNo;
+
+    public void disp() {
+        System.out.println("社員名:" + empName);
+        System.out.println("社員番号:" + empNo);
+    }
+}
---
+++ src/chapter_07/ch7B/EmpExec.java
@@ -0,0 +1,12 @@
+package chapter_07.ch7B;
+
+import chapter_07.ch7A.Employee;
+
+public class EmpExec {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.empName = "福山雅治";
+        emp.empNo = 1000;
+        emp.disp();
+    }
+}
