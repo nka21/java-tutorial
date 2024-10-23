a31d3f6a76b9ff4e096577b0366c5b7838321129
nka21 <naoki.nico@gmail.com>
2024-10-23 20:05:31 +0900

    コメントの記述

--- src/chapter_04/tasks_02/TriangleExec.java
+++ src/chapter_04/tasks_02/TriangleExec.java
@@ -3,10 +3,12 @@ package chapter_04.tasks_02;
 public class TriangleExec {
     public static void main(String[] args) {
         if (args.length > 0) {
+            // 入力値があったとき
             int inputRows = Integer.parseInt(args[0]);
             if (inputRows > 0) {
                 System.out.println("Input number: " + inputRows);
 
+                // 三角形作るロジック
                 for (int i = 0; i < inputRows; i++) {
                     for (int j = 0; j <= i; j++) {
                         System.out.print("*");
@@ -14,10 +16,12 @@ public class TriangleExec {
                     System.out.println("");
                 }
             } else {
+                // 入力値があったけれど、0以下の数字だった場合
                 System.out.println("Input number: " + inputRows);
                 System.out.println("Please input again.");
             }
         } else {
+            // そもそも入力値に値がなかった場合
             System.out.println("Please input one number.");
         }
     }
